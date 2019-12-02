60976c3e7da77b734fceb40a66edbeab6ccc100c
Julian Lires <jlires@uc.cl>
2019-12-02 17:24:52 -0300

    Reordered components. Changed menu style

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -1,17 +1,12 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-func _on_PlayGame_pressed():
+func _on_NewGame_pressed():
 	Global.goto_scene("res://Levels/Level 1.tscn")
-	pass # Replace with function body.
 
 
 func _on_Quit_pressed():
@@ -21,3 +16,37 @@ func _on_Quit_pressed():
 func _on_Instructions_pressed():
 	# TODO: This function doing the same playgame button
 	pass # Replace with function body.
+	
+
+
+## Stylistic hover ##
+
+func _on_PlayGame_mouse_entered():
+	$VBoxContainer/PlayGame/Line2D.show()
+
+func _on_PlayGame_mouse_exited():
+	$VBoxContainer/PlayGame/Line2D.hide()
+
+
+func _on_Instructions_mouse_entered():
+	$VBoxContainer/Instructions/Line2D.show()
+
+
+func _on_Instructions_mouse_exited():
+	$VBoxContainer/Instructions/Line2D.hide()
+
+
+func _on_NewGame_mouse_entered():
+	$VBoxContainer/NewGame/Line2D.show()
+
+
+func _on_NewGame_mouse_exited():
+	$VBoxContainer/NewGame/Line2D.hide()
+
+
+func _on_Quit_mouse_entered():
+	$VBoxContainer/Quit/Line2D.show()
+
+
+func _on_Quit_mouse_exited():
+	$VBoxContainer/Quit/Line2D.hide()
