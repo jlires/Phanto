489d3a867c83ef99115c7421993e99383d9039b2
Julian Lires <jlires@uc.cl>
2019-12-02 23:04:33 -0300

    Added door  to MainMenu in Instructions level

--- Menus/Instructions2.gd
+++ Menus/Instructions2.gd
@@ -4,3 +4,14 @@ onready var _charge_bar = $Player/CanvasLayer/ExperienceBar
 func _ready():
 	_charge_bar.initialize($Player.light_load, $Player.light_load)
 	pass # Replace with function body.
+	
+	
+func _input(event):
+	if Input.is_action_just_pressed("ui_grab"):				
+		var bodies = $Map/Door/Area2D.get_overlapping_bodies()
+		for body in bodies:
+			if body.name == "Player" and not $Map/Door.open:
+				$Map/Door.open()
+				print("Main Menu")
+				yield(get_tree().create_timer(2), "timeout")
+				Global.goto_scene("res://Menus/MainMenu.tscn")
